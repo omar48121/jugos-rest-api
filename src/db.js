6a41b69b2ab1b9b317b76a2dedb2e685e6b25d15
Omar Rodriguez <orodriguez.ordl@gmail.com>
2023-03-27 22:56:21 -0600

    prod mode

--- src/db.js
+++ src/db.js
@@ -1,11 +1,12 @@
 import { createPool } from "mysql2/promise";
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from './config.js';
 
 export const pool = createPool({
-    host: 'vup.h.filess.io',
-    database: 'jugosnaty_strongmine',
-    port: '3306',
-    user: 'jugosnaty_strongmine',
-    password: 'd12f1f2428cf4dce0dda150f1d28fda10db970c5'
+    host: DB_HOST,
+    database: DB_DATABASE,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD
 });
 /* export const pool = createPool({
     host: '127.0.0.1',
