80116ba7f3437bbbfbe4ef668bbd450c46e09de3
Omar Rodriguez <orodriguez.ordl@gmail.com>
2023-04-12 12:02:07 -0600

    logs date fixed 2

--- src/controllers/logs.controller.js
+++ src/controllers/logs.controller.js
@@ -3,6 +3,17 @@ import { pool } from '../db.js';
 export const getLogs = async (req, res) => {
     try {
         const [rows] = await pool.query('SELECT * FROM logs');
+
+        const transformedRows = rows.map((obj) => {
+            const fecha = new Date(obj.date);
+            const fechaMenos6Horas = new Date(fecha.getTime() - (6 * 60 * 60 * 1000));
+            
+            return ({
+                ...obj,
+                date: fechaMenos6Horas.toISOString()
+            })
+        });
+
         res.json(rows);
     } catch (error) {
         return res.status(500).json({
