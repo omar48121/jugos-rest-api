b154cf1aefbeae8684923895b1076240fe0349e4
Omar Rodriguez <orodriguez.ordl@gmail.com>
2023-04-12 11:44:01 -0600

    logs date fixed

--- src/controllers/logs.controller.js
+++ src/controllers/logs.controller.js
@@ -1,9 +1,23 @@
 import { pool } from '../db.js';
 
+// export const getLogs = async (req, res) => {
+//     try {
+//         const [rows] = await pool.query('SELECT * FROM logs');
+//         res.json(rows);
+//     } catch (error) {
+//         return res.status(500).json({
+//             message: "Something went wrong"
+//         });
+//     }
+// }
 export const getLogs = async (req, res) => {
     try {
         const [rows] = await pool.query('SELECT * FROM logs');
-        res.json(rows);
+        const logs = rows.map(log => ({
+            ...log,
+            date: new Date(log.date).toLocaleString()
+        }));
+        res.json(logs);
     } catch (error) {
         return res.status(500).json({
             message: "Something went wrong"
